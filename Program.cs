2dd4ef2c153dffaa6f85f28881e7907c94b4453d
danieljam86 <danie@DESKTOP-BISF0DM>
2022-05-24 05:26:48 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿int valor = 0;
+int[] array = {1, 2, 3, 4, 5};
+int resto = 0;
+
+foreach(int i in array)
+{
+    resto = i % 2;
+    if(resto == 0)
+    {
+        valor += i;
+        Console.WriteLine(valor);
+    }
+    
+}
+
+Console.WriteLine("O valor total de números pares é: " +valor);
